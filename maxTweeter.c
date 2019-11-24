b10b9e88f78fa392b0cec9b4685331b12f2a070c
brycekroencke <brycekroencke@gmail.com>
2019-11-24 13:55:32 -0800

    Update maxTweeter.c
    updated file structure

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,14 +1,33 @@
+
+//Included packages
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Given Definitions
 #define MAX_CSV_LINE_CHARS 1024
 #define MAX_CSV_FILE_LEN 20000
+
+//Definitions based on Twitters limitations
 #define MAX_CHARS_IN_NAME 50
 #define MAX_CHARS_IN_TWEET 280
 
-
-//Make an array of tweeters and sort by tweet_count
-
+//Struct used to keep track of each tweeter and their tweet count
 struct Tweeter {
-    char name[MAX_CHARS_IN_NAME];
-    int tweet_count;
+  char name[MAX_CHARS_IN_NAME];
+  int tweet_count;
 };
 
-struct Tweeter tweeter_array[MAX_CSV_FILE_LEN];
+int main(int argc, char *argv[]) {
+  struct Tweeter tweeter_array[MAX_CSV_FILE_LEN];
+  for (int i = 0; i < MAX_CSV_FILE_LEN; i++) {
+    struct Tweeter new_tweeter = {"", 0};
+    tweeter_array[i] = new_tweeter;
+  }
+
+  FILE *file_path = fopen(argv[1], "r");
+
+  if (!file_path) {
+      exit(1);
+  }
+}
